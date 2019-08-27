538624eaf7d3cc6003d0ca7e9f60a09ad4cc34a0
Umar Hayat Khan <50590127+uhayatkhan@users.noreply.github.com>
2019-08-28 00:55:15 +0500

    Update ci.rb

--- .circleci/ci.rb
+++ .circleci/ci.rb
@@ -1,28 +1,43 @@
-
 require 'net/http'
 require 'json'
-#require 'async/await'
-#include Async::Await
 
 ORGANIZATION = 'uhayatkhan';
 PROJECT = 'circleci-demo-ruby-rails';
-# This is the environment variable added via CircleCI UI
+
 TOKEN = ENV['CIRCLE_API_TOKEN'] #process.env.CIRCLE_API_TOKEN;
 BRANCH = 'master';
 URL = `https://circleci.com/api/v1.1/project/github/#{ORGANIZATION}/#{PROJECT}/tree/#{BRANCH}?circle-token=#{TOKEN}&shallow=true&filter=running`;
 
-# This is a build-in environment variable
-CURRENTBUILDNUM = ENV['CIRCLE_BUILD_NUM'] #process.env.CIRCLE_BUILD_NUM;  
+INTERVAL = 20;
+CURRENTBUILDNUM = ENV['CIRCLE_BUILD_NUM'] #ENV['CIRCLE_BUILD_NUM']
 JOB = 'build';
-INTERVAL = 20000;
-  
+
 def  wait
-  uri = URI('https://circleci.com/api/v1.1/project/github/uhayatkhan/circleci-demo-ruby-rails/tree/master?circle-token=ce62040f14d50d1a16390838d47b2379c40965be&shallow=true&filter=running')
+  uri = URI('https://circleci.com/api/v1.1/project/github/uhayatkhan/circleci-demo-ruby-rails/tree/master?circle-token=ce62040f14d50d1a16390838d47b2379c40965be&shallow=true')
   response = Net::HTTP.get(uri)
   builds = JSON.parse(response)
-  print(builds)
+  print(builds[0]['workflows'])
+
+  if builds[0].has_key? 'workflows'
+  	if builds[0]['workflows']['job_name'] == JOB
+  		isSameJob = true
+  	end
+  end
+  print(builds[0]['build_num'])
+  print(Integer(CURRENTBUILDNUM))
+  if (Integer(builds[0]['build_num']) != Integer(CURRENTBUILDNUM))
+  	isDifferentBuild = true
+  end
+
+  if isSameJob && isDifferentBuild
+  	print("Another staging deployment already running, will check again in #{INTERVAL} seconds...")
+  	sleep INTERVAL
+    puts wait
+  else
+  	print("No other staging deployment already running, build started without wait.")
+  end 
+
 end
 
-wait
-
+puts wait
 
