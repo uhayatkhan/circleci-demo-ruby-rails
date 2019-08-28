78a739a26a18945523ef47c2fa5753f98d8bf1bd
Umar Hayat Khan <50590127+uhayatkhan@users.noreply.github.com>
2019-08-28 12:37:45 +0500

    Update ci.rb

--- .circleci/ci.rb
+++ .circleci/ci.rb
@@ -18,7 +18,7 @@ print(ENV['CIRCLE_BUILD_NUM'])
 JOB = 'build';
 
 def  wait
-  uri = URI('https://circleci.com/api/v1.1/project/github/uhayatkhan/circleci-demo-ruby-rails/tree/master?circle-token=ce62040f14d50d1a16390838d47b2379c40965be&shallow=true')
+  uri = URI('https://circleci.com/api/v1.1/project/github/uhayatkhan/circleci-demo-ruby-rails/tree/master?circle-token=ce62040f14d50d1a16390838d47b2379c40965be&shallow=true&filter=running')
   response = Net::HTTP.get(uri)
   builds = JSON.parse(response)
 
