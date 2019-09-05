3692da3d4bfea1706071b683de4b2abd583a2a38
Umar Hayat Khan <50590127+uhayatkhan@users.noreply.github.com>
2019-09-05 18:47:00 +0500

    Update ci.rb

--- .circleci/ci.rb
+++ .circleci/ci.rb
@@ -1,14 +1,16 @@
 require 'net/http'
 require 'json'
 
-
-
+ORG= "uhayatkhan"
+PROJECT = "circleci-demo-ruby-rails"
 INTERVAL = 20;
 CURRENTBUILDNUM = ENV['CIRCLE_BUILD_NUM'] #ENV['CIRCLE_BUILD_NUM']
 JOB = 'build';
+BRANCH = "master"
+TOKEN = ENV['CIRCLE_API_TOKEN']
 
 def  wait
-  uri = URI.parse('https://circleci.com/api/v1.1/project/github/uhayatkhan/circleci-demo-ruby-rails/tree/master?circle-token=ce62040f14d50d1a16390838d47b2379c40965be&shallow=true&filter=running')
+  uri = URI.parse('https://circleci.com/api/v1.1/project/github/#{ORG}/#{PROJECT}/tree/#{BRANCH}?circle-token=#{TOKEN}&shallow=true&filter=running')
   response = Net::HTTP.get(uri)
   builds = JSON.parse(response)
 
